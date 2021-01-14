899322e0b757618743bb7fb5bbffe57018c780f7
zutheme <hatazu@gmail.com>
2021-01-14 15:10:56 +0700

    cart

--- wp-content/themes/hatazu_fashion/layouts/menu-mobile.php
+++ wp-content/themes/hatazu_fashion/layouts/menu-mobile.php
@@ -58,12 +58,12 @@
                             </a> -->
                         </div>
                         <div class="indicator indicator--mobile">
-                            <a href="cart.html" class="indicator__button">
+                            <a href="javascript:void(0);" class="indicator__button">
                                 <span class="indicator__area">
                                     <svg width="20px" height="20px">
                                         <use xlink:href="<?php echo get_template_directory_uri()  . '/images/sprite.svg'; ?>#cart-20"></use>
                                     </svg>
-                                    <span class="indicator__value">0</span>
+                                    <span class="indicator__value"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
                                 </span>
                             </a>
                         </div>
